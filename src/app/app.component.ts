84bff9030fbe44dd70d8b1aa6114c18900972908
anshumantsagar <anshumantsagar@gmail.com>
2020-08-24 19:38:22 +0530

    todo app in angular 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+  myInput:string;
+  allTasks= [] ;
+
+  addTask(){
+    if(this.allTasks[0] == undefined) {
+      this.allTasks.splice(0,1)
+    }
+    if (this.validation() == true) {
+      let data = {
+        taskHeading: this.myInput,
+        checked: false
+      }
+      this.allTasks.push(data);
+      this.myInput = '';
+    }
+  }
+
+  validation = () => {
+    if (this.myInput !== '' || null || undefined) {
+      return true;
+    }
+  }
+
+  deleteTask = (index) => {
+    this.allTasks.splice(index,1);
+  }
+  checkUncheck = (index) => {
+    let tempArr:any = this.allTasks;
+    this.allTasks[index].checked = !this.allTasks[index].checked;
+    this.addTask = tempArr;
+  }
 }
